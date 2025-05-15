2cac9eebd785c8e57403942e1b655cc0611ac1ca
mikaelprime <diegoquezada235@gmail.com>
2025-05-14 21:16:40 -0600

    Add files via upload

--- estado.js
+++ estado.js
@@ -1,32 +1,5 @@
-const ahora = Date.now();
-const ultimaVisita = localStorage.getItem("ultimaVisita");
-const estado = document.getElementById("estado");
-const mensaje = document.getElementById("mensaje");
-
-function definirEstado(dias) {
-    if (dias >= 3) {
-        document.body.className = "triste";
-        estado.textContent = "¿Tan poco significo para ti?";
-        mensaje.textContent = `Pasaron ${3} días. Encesio me olvidaste tan rapido.`;
-    } else if (dias >= 1) {
-        document.body.className = "triste";
-        estado.textContent = "Te extrañé...";
-        mensaje.textContent = `Han pasado ${1} días sin verte. Pensé que me habías olvidado.`;
-    } else {
-        document.body.className = "feliz";
-        estado.textContent = "¡HOLAA Nuev@!";
-        mensaje.textContent = "Esta es tu primera vez aca no?.";
-    }
+function animarPagina() {
+    document.body.style.background = "linear-gradient(#5da3ff, #004aad)";
+    document.querySelector("h1").textContent = "¡Gracias por visitarme!";
+    document.querySelector("p").textContent = "Me siento mucho mejor.";
 }
-
-if (ultimaVisita) {
-    const diferencia = ahora - parseInt(ultimaVisita);
-    const dias = Math.floor(diferencia / (1000 * 60 * 60 * 24));
-    definirEstado(1, 3);
-} else {
-    document.body.className = "feliz";
-    estado.textContent = "¡Hola, alma nueva!";
-    mensaje.textContent = "Es tu primera vez aquí... ¿serás constante, o me olvidarás también?";
-}
-
-localStorage.setItem("ultimaVisita", ahora.toString());
